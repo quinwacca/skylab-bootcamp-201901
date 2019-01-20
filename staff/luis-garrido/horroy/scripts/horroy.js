fa288a9490b641413b078baf4386ed3a7684a362
quinwacca <37936498+quinwacca@users.noreply.github.com>
2019-01-20 21:06:37 +0100

    minor updates

--- staff/luis-garrido/horroy/scripts/horroy.js
+++ staff/luis-garrido/horroy/scripts/horroy.js
@@ -8,15 +8,15 @@ function Horroy() {
 }
 
 Horroy.from = function (value) {
-    var horr = new Horroy
+    var horr = new Horroy;
 
-    horr.length = value.length
+    horr.length = value.length;
 
-    if (typeof value === 'string')
+    if (typeof value === 'string') 
         for (var i = 0; i < value.length; i++)
-            horr[i] = value[i]
+            horr[i] = value[i];
 
-    return horr
+    return horr;
 };
 
 Horroy.prototype.push = function (value) {
@@ -37,4 +37,34 @@ Horroy.prototype.toString = function() {
     string += this[this.length - 1];
 
     return string;
+};
+
+/**
+ * ---------------------   FILL   -----------------------------
+ * 
+ * Fills an array from one position to other.
+ * 
+ * @param {Array} array 
+ * @param {*} value 
+ * @param {number} start 
+ * @param {number} end 
+ * 
+ * @throws {Error} - If too many arguments (> 4)
+ * @throws {TypeError} - If array is not an array
+ */
+
+Horroy.prototype.fill = function(value, start, end) {
+    // if (arguments.length > 4) throw Error('too many arguments');
+    // expect(arguments.length).toBeLessThanOrEqual(4);
+    // expect(arguments[0] instanceof Array).toBe(true);
+    // if (!(array instanceof Array))
+    //     throw new TypeError(array + ' is not an array');
+
+    start = start === undefined ? 0 : (start < 0 ? array.length + start : start);
+    end = end === undefined ? array.length : (end < 0 ? array.length + end : end);
+debugger
+    for (var i = start; i < end; i++)
+        this[i] = value;
+
+    return array;
 };
--- staff/luis-garrido/horroy/scripts/horroy.spec.js
+++ staff/luis-garrido/horroy/scripts/horroy.spec.js
@@ -14,6 +14,86 @@ describe('Horroy', function() {
             var horr = Horroy.from(string);
 
             expect(horr.toString()).toBe(string.split('').toString()); // h,o,l,a, ,m,u,n,d,o
-        })
+        });
+    });
+
+    describe('push', function() {
+        it('should push the correct value', function() {
+            var horroy = new Horroy('pizza','cheese','lettuce');
+            var pushing = 'tomato';
+        
+            horroy.push(pushing);
+        
+            expect(horroy[3]).toBe(pushing);
+        });
+
+        it('should increment his length by one', function() {
+            var horroy = new Horroy('pizza','cheese','lettuce');
+            var originalLength = horroy.length;
+
+            var pushing = 'tomato';
+
+            horroy.push(pushing);
+        
+            expect(horroy.length).toBe(originalLength+1);
+        });
+    });
+    describe('forEach', function() {
+        it('should iterate every value of the horroy', function() {
+            var horroy = new Horroy('pizza','cheese','lettuce');
+            var res = '';
+            var expectedString = 'pizzacheeselettuce';
+
+            horroy.forEach(function(item){
+                res+=item;
+            });
+
+            expect(res).toBe(expectedString);
+        });
+        it('should return undefined', function(){
+            horroy = new Horroy('pizza','cheese','lettuce');
+            
+            var expectedUndefined = undefined;
+
+            var res = horroy.forEach(function(item){
+                res+=item;
+            });
+
+            expect(res).toBe(expectedUndefined);
+        });
+    });
+    describe('toString', function() {
+        it('should return the string with all items separated by commas', function() {
+            horroy = new Horroy('pizza','cheese','lettuce');
+
+            var expectedString = 'pizza,cheese,lettuce';
+
+            var res = horroy.toString();
+
+            expect(res).toBe(expectedString);
+        });
+        // it('should return an empty string', function() {
+        //     horroy = new Horroy();
+
+        //     var expectedString = '';
+
+        //     var res = horroy.toString();
+
+        //     expect(res).toBe(expectedString);
+        // });
+    });
+    describe('fill', function() {
+        it('should fill horroy with the same value, all arguments correct', function() {
+            horroy = new Horroy('pizza','cheese','lettuce','bacon','tomato');
+                        
+            var res = horroy.fill(0, 0, 2);
+            
+            var expected = [0, 0, 'lettuce','bacon','tomato'];
+            
+            expect(res).toBe(expected);
+            // if (res !== arr) throw Error('array and result should be the same');
+            // if (res.toString() !== expected.toString()) throw Error('result should be the one expected');
+            // if (arr.toString() !== expected.toString()) throw Error('array should have been changed to the one expected');
+        });
     });
 });
